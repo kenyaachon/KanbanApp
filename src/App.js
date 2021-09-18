007cb66498a2cd2ba554dd6e3c1371c68dd73cfa
mosesmbugua <swahili1963@gmail.com>
2021-09-18 10:04:40 -0700

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React from "react";
+import KanbanBoard from "./KanbanBoard";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+let cardsList = [
+  {
+    id: 1,
+    title: "Read the Book",
+    description: "I should read the whole book",
+    status: "in-progress",
+    tasks: [],
+  },
+  {
+    id: 2,
+    title: "Write some code",
+    description: "Code along with the samples in the book",
+    status: "todo",
+    tasks: [
+      {
+        id: 1,
+        name: "ContactList Example",
+        done: true,
+      },
+      {
+        id: 2,
+        name: "Kanban Example",
+        done: false,
+      },
+      {
+        id: 3,
+        name: "My own experiments",
+        done: false,
+      },
+    ],
+  },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  React.render(
+    <KanbanBoard cards={cardsList} />,
+    document.getElementById("root")
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+import "./index.css";
+// import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import KanbanBoard from "./KanbanBoard";
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+let cardsList = [
+  {
+    id: 1,
+    title: "Read the Book",
+    description: "I should read the whole book",
+    status: "in-progress",
+    tasks: [],
+  },
+  {
+    id: 2,
+    title: "Write some code",
+    description: "Code along with the samples in the book",
+    status: "todo",
+    tasks: [
+      {
+        id: 1,
+        name: "ContactList Example",
+        done: true,
+      },
+      {
+        id: 2,
+        name: "Kanban Example",
+        done: false,
+      },
+      {
+        id: 3,
+        name: "My own experiments",
+        done: false,
+      },
+    ],
+  },
+];
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <KanbanBoard cards={cardsList} />,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
