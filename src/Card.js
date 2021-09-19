2b3f1b6a59e02eddf49b29405c28e7990c75fcba
mosesmbugua <swahili1963@gmail.com>
2021-09-18 23:19:05 -0700

    Card Color via Inline Styling

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import CheckList from "./CheckList";
+import marked from "marked";
 
 class Card extends Component {
   constructor() {
@@ -9,24 +10,49 @@ class Card extends Component {
     };
   }
 
+  toggleDetails() {
+    this.setState({ showDetails: !this.state.showDetails });
+  }
+
+  //this how we add a colored line next to each card
+  sideColors() {
+    return {
+      position: "absolute",
+      zIndex: -1,
+      top: 0,
+      bottom: 0,
+      left: 0,
+      width: 7,
+      backgroundColor: this.props.color,
+    };
+  }
   render() {
     let cardDetails;
+    //determines if the toggle has been pressed
+    //in order to make our styling on html work we needed the dangerouslySetInnerHTML
     if (this.state.showDetails) {
       cardDetails = (
         <div className="card__details">
-          {this.props.description}
+          <span
+            dangerouslySetInnerHTML={{ __html: marked(this.props.description) }}
+          />
           <CheckList cardId={this.props.id} tasks={this.props.tasks} />
         </div>
       );
     }
 
+    let sideColor = this.sideColors();
+    //when the toggle is displayed, it changes the state of showDetails
     return (
       <div className="card">
+        <div style={sideColor} />
         <div
-          className="card__title"
-          onClick={() =>
-            this.setState({ showDetails: !this.state.showDetails })
+          className={
+            this.state.showDetails
+              ? "card__title card__title--is-open"
+              : "card__title"
           }
+          onClick={this.toggleDetails.bind(this)}
         >
           {this.props.title}
         </div>
